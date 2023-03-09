121f90455eac6304a55a7aff718fa9495605d77d
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-03-10 01:52:33 +0800

    import react is not needed any more

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function AboutPage () {
   return <h1>About</h1>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import Head from 'next/head';
 import styled, { createGlobalStyle } from 'styled-components';
 import 'three';
