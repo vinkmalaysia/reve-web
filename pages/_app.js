f55ef1ad2072013d6386755ce5fc041f2e3cb03c
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-03-10 01:11:28 +0800

    Fix redux store warning according to next-redux-wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { withRouter } from 'next/router';
@@ -10,19 +11,21 @@ import theme from '../theme';
 const NormalizeCSS = createGlobalStyle` ${require('!!raw-loader!@csstools/normalize.css').default} `;
 const AppGlobalCSS = createGlobalStyle` ${require('!!raw-loader!../src/stylesheets/global.css').default} `;
 
-const MyApp = ({ Component, pageProps, router }) => {
+const MyApp = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Helmet
-        htmlAttributes={{ lang: 'en' }}
-      />
-      <NormalizeCSS />
-      <AppGlobalCSS />
-      <Component router={router} {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+        />
+        <NormalizeCSS />
+        <AppGlobalCSS />
+        <Component router={props.router} {...props.pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
-export default wrapper.withRedux(
-  withRouter(MyApp),
-);
+export default withRouter(MyApp);
