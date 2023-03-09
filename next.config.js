1ffc91c11553201d3b4213bc3de33664f30126c4
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-03-09 23:54:29 +0800

    Removed babel to use nextjs builtin swc

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
-const assetPath = isProd ? (process.env.CDN_HOST || undefined) : undefined;
 
 module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
   poweredByHeader: false,
   compress: false, // Let express handle compression
-  assetPrefix: assetPath,
   webpack: (config, { dev }) => {
-    // CDN prefix
-    config.output.publicPath = `${assetPath}${config.output.publicPath}`;
-
     // SVG loader
     config.module.rules.push({
       test: /\.svg$/,
