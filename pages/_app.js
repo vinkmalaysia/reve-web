841c3ba44294c3527325f255b9d1653b3175c68a
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-03-10 18:43:02 +0800

    Global css is now supported by next

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ import { wrapper } from 'src/store';
 import theme from '../theme';
 
 // Global Styles
-const NormalizeCSS = createGlobalStyle` ${require('!!raw-loader!@csstools/normalize.css').default} `;
-const AppGlobalCSS = createGlobalStyle` ${require('!!raw-loader!../src/stylesheets/global.css').default} `;
+import '@csstools/normalize.css';
+import 'src/stylesheets/global.css';
 
 const MyApp = ({ Component, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
@@ -20,8 +20,6 @@ const MyApp = ({ Component, ...rest }) => {
         <Helmet
           htmlAttributes={{ lang: 'en' }}
         />
-        <NormalizeCSS />
-        <AppGlobalCSS />
         <Component router={props.router} {...props.pageProps} />
       </ThemeProvider>
     </Provider>
